906c32d92a13034d130cbbb32f584eb419cbc671
Adrienne Tran <adrienne.tran@gmail.com>
2023-09-16 15:31:25 -0700

    worked on opinion backend routes

--- app/forms/opinions_form.py
+++ app/forms/opinions_form.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-from app.models import Opinions
+from wtforms import TextAreaField
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Opinion
 
 
+def opinion_length(form, field):
+    content = field.data
 
-class Opinions(FlaskForm):
-    content = TextField('Opinion', validators=[DataRequired()])
+    if len(content) < 10:
+        raise ValidationError("Your opinion must be more than 10 characters")
+    
+    if len(content) > 5000:
+        raise ValidationError("Your opinion cannot be more than 5000 characters")
+
+
+class OpinionForm(FlaskForm):
+    content = TextAreaField('Opinion', validators=[DataRequired(), opinion_length])
