c798058b6738f584e747a62ab8b7a90813078cca
Adrienne Tran <adrienne.tran@gmail.com>
2023-09-16 18:32:56 -0700

    added backend routes for opinions and stocks

--- app/api/stocks_routes.py
+++ app/api/stocks_routes.py
@@ -7,9 +7,29 @@ stock_routes = Blueprint('stocks', __name__)
 
 @stock_routes.route('/')
 @login_required
-def stocks():
+def get_stocks():
     """
     Query for all stocks and returns them in alist of stock tickers 
     """
     stocks = Stock.query.all()
-    return {'stocks': [stock.to_dict() for stock in stocks]}
+    return {'stocks': {stock.ticker: stock.to_dict() for stock in stocks}}
+
+@stock_routes.route('/<string:filter>')
+@login_required
+def get_filtered_stocks(filter):
+    """
+    Query for stocks based on name or ticker
+    """
+    search = f'%{filter}%'
+    print(search)
+
+    stocks_by_ticker = Stock.query.filter(Stock.ticker.like(search))
+    stocks_by_name = Stock.query.filter(Stock.name.like(search.upper()))
+
+    stocks = {stock.ticker: stock.to_dict() for stock in stocks_by_ticker}
+
+    for stock in stocks_by_name:
+        stocks[stock.ticker] = stock.to_dict()
+     
+    return {'stocks': stocks}
+
