586e7e4c70d9647d0bba0b1197676c1221854514
Kevin Sy <kevsy@ucdavis.edu>
2023-09-20 23:53:02 -0700

    creation of portfolio and portfolioStock Reducer but haven't fully made it

---
+++ react-app/src/store/portfolio.js
@@ -0,0 +1,17 @@
+//Action Type
+
+
+
+//Action Creator
+
+
+//Thunk
+
+
+//Reducer
+
+const initialState = {}
+
+export default function portfolioReducer(state = initialState, action) {
+    let newState = {...state}
+}
