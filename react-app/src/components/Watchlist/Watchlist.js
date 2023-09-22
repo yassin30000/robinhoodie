12a7a51ccb1266e887dc3e45e0f5d2659bcd9bfc
Yassin Tantawy <yassintantawy@Yassins-MacBook-Pro.local>
2023-09-21 23:30:48 -0700

    awesome css and functionality for watchlists

--- react-app/src/components/Watchlist/Watchlist.js
+++ react-app/src/components/Watchlist/Watchlist.js
@@ -2,19 +2,26 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUserWatchlists } from "../../store/watchlists";
 import "../Watchlist/Watchlist.css";
-
+import { useState } from "react";
 
 
 function Watchlist() {
     const dispatch = useDispatch();
     const userWatchlistsData = useSelector((state) => state.watchlists.userWatchlists);
     const userWatchlists = userWatchlistsData ? Object.values(userWatchlistsData.watchlists) : []
+    const [rotatedItems, setRotatedItems] = useState({}); // Maintain state for rotated items
 
-    console.log("LISTS!!!!!!!!!!!: ", userWatchlists)
     useEffect(() => {
         // Fetch the user's watchlists when the component mounts
         dispatch(fetchUserWatchlists()); // Replace 'userId' with the actual user's ID
     }, [dispatch]);
+
+    const toggleRotate = (id) => {
+        setRotatedItems((prevState) => ({
+            ...prevState,
+            [id]: !prevState[id], // Toggle the rotation status for the item
+        }));
+    };
 
     return (
         <>
@@ -27,7 +34,10 @@ function Watchlist() {
                 <div id="list-content">
                     {userWatchlists &&
                         userWatchlists.map((watchlist) => (
-                            <div id="list-item" key={watchlist.id}>
+                            <div id="list-item"
+                                key={watchlist.id}
+                                onClick={() => toggleRotate(watchlist.id)}
+                            >
 
 
 
@@ -37,7 +47,8 @@ function Watchlist() {
                                 </div>
                                 <div id="right-side">
                                     <span class="material-icons dots-16">more_horiz</span>
-                                    <span class="material-icons arrow">expand_more</span>
+                                    <span
+                                        class={rotatedItems[watchlist.id] ? "material-icons arrow rotate-180" : "material-icons arrow rotate-0"}>expand_more</span>
                                 </div>
 
                             </div>
