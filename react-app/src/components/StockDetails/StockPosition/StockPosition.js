59783333a5163d578be08f8eb71160a7d5eeafc4
Yassin Tantawy <116838492+yassin30000@users.noreply.github.com>
2023-09-27 11:49:18 -0700

    Update StockPosition.js

--- react-app/src/components/StockDetails/StockPosition/StockPosition.js
+++ react-app/src/components/StockDetails/StockPosition/StockPosition.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import "./StockPosition.css";
-import { useSelector } from 'react-redux';
 
 
 function StockPosition({ latestPrice, stocks_owned_by_user }) {
